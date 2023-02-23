3f26647b6292336bcbbb366115881f4b6eb6acf3
gopal das <gopal1409@gmail.com>
2023-02-23 18:43:34 +0530

    terraform user data

--- a4.ec2.tf
+++ a4.ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "name" {
   ami = data.aws_ami.testimage.id
   instance_type = var.instance_type
+  user_data = file("${path.module}/app-install.sh")
   ###any resource which accept multiple values it should inside square bracket or array
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id,aws_security_group.vpc-http.id ]
   tags = {
