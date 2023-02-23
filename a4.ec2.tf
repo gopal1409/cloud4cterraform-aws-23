a999b9fe3e5aae49702a0e7db7c7333b6909bb17
gopal das <gopal1409@gmail.com>
2023-02-23 21:00:49 +0530

    opt in status

--- a4.ec2.tf
+++ a4.ec2.tf
@@ -1,13 +1,23 @@
+
+##3it will query which az are avaialbe by default
+#why we are query using a for_each loop we can deploy an instance on those az 
+data "aws_availability_zones" "my_zones" {
+   filter {
+     name = "opt-in-status"
+     values = ["opt-in-not-required"]
+   }
+}
 resource "aws_instance" "name" {
   ami = data.aws_ami.testimage.id
   instance_type = var.instance_type
   user_data = file("${path.module}/app1-install.sh")
   ###any resource which accept multiple values it should inside square bracket or array
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id,aws_security_group.vpc-http.id ]
-  count = 2
+  for_each = toset(data.aws_availability_zones.my_zones.names)
+  availability_zone = each.key 
   #whole number
   tags = {
-    "name" = "Count-Demo-${count.index}"
+    "name" = "instance-${each.value}"
   }
 }
 
@@ -16,9 +26,10 @@ resource "aws_instance" "name" {
   name = var.user_name[count.index]
   #user neo.0 neo.1 neo.2
 }*/
-
+/*
 resource "aws_iam_user" "user" {
   for_each = toset(var.user_name)
   name = each.value
   #user neo.0 neo.1 neo.2
 }
+*/
