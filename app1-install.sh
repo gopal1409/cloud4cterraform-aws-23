c6293077686e7551b097379c1f45726693244345
gopal das <gopal1409@gmail.com>
2023-02-23 18:53:12 +0530

    terraform user data

--- app1-install.sh
+++ app1-install.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl enable apache2
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl enable httpd 
 sudo systemctl start httpd 
 sudo echo '<h1>Welcome to my site</h1>' | sudo tee /var/www/html/index.html 
 sudo mkdir /var/www/html/app1
+sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/metadata.html
