cf69dcd31c102bf7b3444d5510de869afef82e6a
gopal das <gopal1409@gmail.com>
2023-02-24 20:18:56 +0530

    vpc with module

--- a3.local.tf
+++ a3.local.tf
@@ -1,9 +1,14 @@
+####terraform has create a block called as local block
 locals {
+    #how do we concatinate this varaible
+    #we created a key and call the varAbile
   owners=var.business_devision
   environment = var.environment
   name = "${var.business_devision}-${var.environment}"
+  ###
+  #name=dev-sap
   common_tags = {
-    owners = locals.owners
-    environment = locals.environment
+    owners = local.owners
+    environment = local.environment
   }
 }
