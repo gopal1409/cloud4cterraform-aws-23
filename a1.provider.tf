984f3af130070076fd8e0a927b678452d3d02496
gopal das <gopal1409@gmail.com>
2023-03-01 18:25:40 +0530

    terraform with remote lock and state file in remote

--- a1.provider.tf
+++ a1.provider.tf
@@ -6,10 +6,19 @@ terraform {
       #version = "~> 4.0"
       #in production always put this version number
     }
+    ###adding backend as s3 for remote state storage
+    
     null = {
       source = "hashicorp/null"
     }
+    
   }
+  backend "s3" {
+      bucket = "terraform-on-aws-for-elb-gopal"
+      key = "terraform.tfstate"
+      region = "us-east-1"
+      dynamodb_table= "dev-project1-vpc"
+    }
 }
 
 #provider block
