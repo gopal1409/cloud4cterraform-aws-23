6a95aaf91ca513cc03ad6ea27e618529103c8e14
gopal das <gopal1409@gmail.com>
2023-03-03 19:43:46 +0530

    terraform testing

--- a2.varaibles.tf
+++ a2.varaibles.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
   description = "Region in which aws resource need to be created"
   type        = string
   #you can have type as string numeric and list
-  default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "instance_type" {
