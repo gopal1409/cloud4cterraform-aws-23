1f9648a045b1491d7de9365af973986f2f461546
gopal das <gopal1409@gmail.com>
2023-03-03 19:43:00 +0530

    terraform testing

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami = "ami-0d5d9d301c853a04a"
-  instance_type = t2.micro 
-  vpc_security_group_ids = [ "value" ]
+  instance_type = "t2.micro" 
+  vpc_security_group_ids = [aws_security_group.instance.id ]
   user_data = <<EOF
   #!/bin/bash 
   echo "Hello,World" > index.html
